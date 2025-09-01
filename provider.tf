683ca6b23693810a88d01bcc762da05ca216244b
Priyanshu mishra <119335221+priyanshumishracs@users.noreply.github.com>
2025-09-01 14:59:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.50"
+      version = ">= 3.64"
     }
     random = {
       source  = "hashicorp/random"
