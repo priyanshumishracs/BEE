23edffb0a63f1c307e826daa070d41253fc21884
Priyanshu mishra <119335221+priyanshumishracs@users.noreply.github.com>
2025-09-01 14:40:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -18,6 +18,11 @@ provider "azurerm" {
       delete_os_disk_on_deletion = true
     }
   }
+
+  client_id       = var.client_id       # fallback to ARM_CLIENT_ID environment variable
+  client_secret   = var.client_secret   # fallback to ARM_CLIENT_SECRET
+  tenant_id       = var.tenant_id       # fallback to ARM_TENANT_ID
+  subscription_id = var.subscription_id # fallback to ARM_SUBSCRIPTION_ID
   # Credentials will come from Jenkins environment variables
   # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID, ARM_SUBSCRIPTION_ID
 }
